d58aff3871a4345bcbc00bdb192d2893e47e3def
jws0806 <102636345+jws0806@users.noreply.github.com>
2025-03-03 11:21:42 +0900

    Title: 키, 몸무게를 기준으로 정렬, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250303/키, 몸무게를 기준으로 정렬/sort-by-height-and-weight.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+#include <iomanip>
+
+using namespace std;
+
+string name[10];
+int height[10];
+double weight[10];
+
+bool cmp(int i, int j) {
+    if(height[i]==height[j]){
+        return weight[i]>weight[j];
+    }
+    else{
+        return height[i]<height[j];
+    }
+}
+
+int main() {
+    int idx[5]; // 정렬을 위한 인덱스 배열
+
+    int n;
+    cin>>n;
+
+    // 입력 받기
+    for (int i = 0; i < n; i++) {
+        cin >> name[i] >> height[i] >> weight[i];
+        idx[i] = i; // 초기 인덱스 설정
+    }
+
+    sort(idx, idx + n, cmp);
+
+    for (int i = 0; i < 5; i++) {
+        int index = idx[i];
+        cout << name[index] <<" "<< height[index] <<" "<< weight[index] << endl;
+    }
+
+    return 0;
+}
