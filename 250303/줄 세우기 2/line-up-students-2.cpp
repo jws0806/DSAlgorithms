3da141064a02b9f3e4a356f48a7f661fb614396e
jws0806 <102636345+jws0806@users.noreply.github.com>
2025-03-03 18:20:05 +0900

    Title: 줄 세우기 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 250303/줄 세우기 2/line-up-students-2.cpp
+++ 250303/줄 세우기 2/line-up-students-2.cpp
@@ -1,35 +1,33 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+
 using namespace std;
 
 int N;
-int h;
-int w;
-bool cmp(pair<int,int>a, pair<int,int>b){
-    if(a.first==b.first){
-        return a.second>b.second;
-    }
-    else{
-        return a.first<b.first;
-    }
+int h, w;
 
+bool cmp(pair<pair<int, int>, int> a, pair<pair<int, int>, int> b) {
+    if (a.first.first == b.first.first) {
+        return a.first.second > b.first.second;
+    }
+    return a.first.first < b.first.first;
 }
 
 int main() {
-    vector<pair<int,int>>v;
+    vector<pair<pair<int, int>, int>> v;
     cin >> N;
 
     for (int i = 0; i < N; i++) {
         cin >> h >> w;
-        v.push_back({h,w});
-    }
-    sort(v.begin(), v.end(),cmp);
-
-    for(int i=0; i<N; i++){
-        cout<<v[i].first<<" "<<v[i].second<<" "<<i+1<<"\n";
+        v.push_back({{h, w}, i + 1});  // 원래 입력된 인덱스를 저장
     }
 
+    sort(v.begin(), v.end(), cmp);
+
+    for (int i = 0; i < N; i++) {
+        cout << v[i].first.first << " " << v[i].first.second << " " << v[i].second << "\n";
+    }
 
     return 0;
 }
