674b2f8f8e3eca940ac7ce70836554a1c1f310a1
jws0806 <102636345+jws0806@users.noreply.github.com>
2025-03-03 10:12:01 +0900

    Title: 개인정보, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 250303/개인정보/personal-info.cpp
+++ 250303/개인정보/personal-info.cpp
@@ -8,28 +8,32 @@ string name[5];
 int height[5];
 double weight[5];
 
+// 이름 기준 오름차순 정렬
 bool cmp_name(int i, int j) {
     return name[i] < name[j];
 }
 
+// 키 기준 내림차순 정렬
 bool cmp_height(int i, int j) {
     return height[i] > height[j];
 }
 
 int main() {
-    int idx[5]; 
+    int idx[5]; // 정렬을 위한 인덱스 배열
 
+    // 입력 받기
     for (int i = 0; i < 5; i++) {
         cin >> name[i] >> height[i] >> weight[i];
-        idx[i] = i; 
+        idx[i] = i; // 초기 인덱스 설정
     }
 
+    // 이름 기준 정렬 및 출력
     sort(idx, idx + 5, cmp_name);
     cout << "name" << endl;
     for (int i = 0; i < 5; i++) {
         int index = idx[i];
         cout << name[index] << " " << height[index] << " "
-             << weight[index] << endl;
+             << fixed << setprecision(1) << weight[index] << endl;
     }
 
     // 키 기준 정렬 및 출력
@@ -38,7 +42,7 @@ int main() {
     for (int i = 0; i < 5; i++) {
         int index = idx[i];
         cout << name[index] << " " << height[index] << " "
-             << weight[index] << endl;
+             << fixed << setprecision(1) << weight[index] << endl;
     }
 
     return 0;
