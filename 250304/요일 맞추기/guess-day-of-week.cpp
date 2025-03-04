e5dd685ee5c7fa508c5b53246304f49764ae0921
jws0806 <102636345+jws0806@users.noreply.github.com>
2025-03-04 16:09:38 +0900

    Title: 요일 맞추기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250304/요일 맞추기/guess-day-of-week.cpp
+++ 250304/요일 맞추기/guess-day-of-week.cpp
@@ -9,35 +9,28 @@ int m1, d1, m2, d2;
 int main() {
     cin >> m1 >> d1 >> m2 >> d2;
 
-    int month[12]={31, 28, 31, 30,31,30,31,31,30,31,30,31};
-    string day[7]={"Mon","Tue","Wed","Thu","Fri","Sat","Sun"};
+    int month[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    string day[7] = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
 
-    int sum_before=0;
-    int sum_after=0;
+    int sum_before = 0, sum_after = 0;
 
-    for(int i=0; i<m1-1; i++){
-        sum_before+=month[i];
-
+    for (int i = 0; i < m1 - 1; i++) {
+        sum_before += month[i];
     }
-    sum_before=sum_before+d1;
+    sum_before += d1;
 
-    for(int i=0; i<m2-1; i++){
-
-        sum_after+=month[i];
-
+    for (int i = 0; i < m2 - 1; i++) {
+        sum_after += month[i];
     }
-    sum_after+=d2;
+    sum_after += d2;
 
-
-    int days=sum_after-sum_before;
-    //cout<<days;
-
-    if(days>0){
-        cout<<day[days%7];
-    }
-    else{
-        int k=7-abs(days)%7;
-        cout<<day[k];
+    int days = sum_after - sum_before;
+    
+    if (days >= 0) {
+        cout << day[days % 7];
+    } else {
+        int k = (7 - abs(days) % 7) % 7;
+        cout << day[k];
     }
 
     return 0;
