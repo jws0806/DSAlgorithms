f6d4183b48dd92860f019834c9de04ace8091f1b
jws0806 <102636345+jws0806@users.noreply.github.com>
2025-03-05 20:21:47 +0900

    Title: 진수 to 진수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250305/진수 to 진수/transformation-of-number-system.cpp
+++ 250305/진수 to 진수/transformation-of-number-system.cpp
@@ -12,27 +12,34 @@ int main() {
     cin >> a >> b;
     cin >> n;
 
-    int sum=0;
+    int sum = 0;
 
-    for(int i=0; i<n.size(); i++){
-        sum+=(n[i]-'0')*pow(a,i);
+    // a 진법 -> 10진법 변환
+    for (int i = 0; i < n.size(); i++) {
+        int digit;
+        if (isdigit(n[i])) digit = n[i] - '0';  // 숫자 0~9 변환
+        else digit = n[i] - 'A' + 10; // A~Z 처리 (예: 16진법 이상)
 
+        sum = sum * a + digit;
     }
-    int num=sum;
+    
+    int num = sum;
+    vector<int> v;
 
-    vector<int>v;
-    while(num>0){
-        v.push_back(num%b);
-        num=num/b;
+    // 10진법 -> b 진법 변환
+    if (num == 0) v.push_back(0); // 0일 경우 예외 처리
+    while (num > 0) {
+        v.push_back(num % b);
+        num /= b;
     }
 
-    reverse(v.begin(),v.end());
+    reverse(v.begin(), v.end());
 
-    for(int i=0; i<v.size(); i++){
-        cout<<v[i];
+    // 출력
+    for (int i = 0; i < v.size(); i++) {
+        if (v[i] < 10) cout << v[i]; // 0~9는 그대로 출력
+        else cout << char('A' + v[i] - 10); // 10 이상이면 A~Z 문자로 변환
     }
-
-    // Please write your code here.
 
     return 0;
 }
