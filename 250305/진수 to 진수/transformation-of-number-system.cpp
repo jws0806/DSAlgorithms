c835571fbc9bf5f5774a77217a0598d2436aedd2
jws0806 <102636345+jws0806@users.noreply.github.com>
2025-03-05 20:14:29 +0900

    Title: 진수 to 진수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250305/진수 to 진수/transformation-of-number-system.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int a, b;
+string n;
+
+int main() {
+    cin >> a >> b;
+    cin >> n;
+
+    int sum=0;
+
+    for(int i=0; i<n.size(); i++){
+        sum+=pow(8,i);
+
+    }
+    int num=sum;
+
+    vector<string>v;
+    while(num>0){
+        v.push_back(to_string(num%b));
+        num=num/b;
+    }
+
+    reverse(v.begin(),v.end());
+
+    for(int i=0; i<v.size(); i++){
+        cout<<v[i];
+    }
+
+    // Please write your code here.
+
+    return 0;
+}
