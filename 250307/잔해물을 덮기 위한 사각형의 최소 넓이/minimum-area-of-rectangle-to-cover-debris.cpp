d5c26949eb5eb972f5b3af4c86e51e4f67465250
jws0806 <102636345+jws0806@users.noreply.github.com>
2025-03-07 10:48:49 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250307/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.cpp
+++ 250307/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.cpp
@@ -9,8 +9,8 @@ int main() {
     cin >> x1[1] >> y1[1] >> x2[1] >> y2[1];
 
     int offset = 1000;
-    for(int i = y1[0] + offset; i <= y2[0] + offset; i++) {
-        for(int j = x1[0] + offset; j <= x2[0] + offset; j++) {
+    for(int i = y1[0] + offset; i < y2[0] + offset; i++) {
+        for(int j = x1[0] + offset; j < x2[0] + offset; j++) {
             v[i][j] = 1;
         }
     }
@@ -20,21 +20,27 @@ int main() {
     int y11=max(y1[0],y1[1]);
     int y22=min(y2[0],y2[1]);
 
-    for(int i = y11 + offset; i <= y22 + offset; i++) {
-        for(int j = x11 + offset; j <= x22 + offset; j++) {
-            v[i][j] = 0;
-        }
-    }
-    
 
     // 남은 영역 카운트
     int cnt = 0;
-    for(int i = y1[0] + offset; i <= y2[0] + offset; i++) {
-        for(int j = x1[0] + offset; j <= x2[0] + offset; j++) {
+    for(int i = y1[0] + offset; i < y2[0] + offset; i++) {
+        for(int j = x1[0] + offset; j < x2[0] + offset; j++) {
             if(v[i][j] == 1) cnt++;
         }
     }
-
-    cout << cnt;
+    if(x11!=x1[0] && y22!=y1[1]){
+        cout<<cnt;
+    }
+    else{
+        if(x11==x1[0] && y22==y1[0]){
+            cout<<0;
+        }
+        else if(x11==x1[0]){
+            
+        }
+        else if(y22==y1[0]){
+            
+        }
+    }
     return 0;
 }
