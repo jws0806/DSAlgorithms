20dd78cfd131dd245bc22f8123ea3cbc81e23106
jws0806 <102636345+jws0806@users.noreply.github.com>
2025-03-08 21:33:30 +0900

    Title: 되돌아오기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250308/되돌아오기/come-back.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+char dir;
+int dist;
+int x=0;
+int y=0;
+
+int main() {
+    cin >> N;
+
+    int time=0;
+    int ans=0;
+
+    for (int i = 0; i < N; i++) {
+        cin >> dir >> dist;
+        for(int j=0; j<dist; j++){
+            if(dir=='N')          y++;
+            else if(dir == 'E')   x++;
+            else if(dir == 'S')   y--;
+            else if(dir == 'W')   x--;
+
+            time++;
+           //cout<<x<<" "<<y<<endl;
+
+            if(x==0 && y==0){
+                ans=time;
+                break;
+            }
+        }
+
+    }
+    cout<<ans;
+
+    // Please write your code here.
+
+    return 0;
+}
