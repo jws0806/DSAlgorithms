47ffd94b201e3365469ffb959cf079f0769348c2
jws0806 <102636345+jws0806@users.noreply.github.com>
2025-03-08 21:24:10 +0900

    Title: 좌우로 움직이는 로봇, Time: 21ms, Memory: 9MB, Status: Wrong Answer - CodeTree

--- 250308/좌우로 움직이는 로봇/robot-moving-from-side-to-side.cpp
+++ 250308/좌우로 움직이는 로봇/robot-moving-from-side-to-side.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
 int n, m;
@@ -7,78 +7,45 @@ int t;
 char d;
 int t_b;
 char d_b;
-long a[1000000];
-long b[1000000];
+vector<int> a, b;
 
 int main() {
     cin >> n >> m;
-    int sumA=0;
-    int T_A=0;
-    int sumB=0;
-    int T_B=0;
-
-    int timeA=0;
-    int timeB=0;
+    int sumA = 0, sumB = 0;
+    int timeA = 0, timeB = 0;
 
     for (int i = 0; i < n; i++) {
         cin >> t >> d;
-        timeA+=t;
-        for(int i=0; i<t; i++){
-            if(d=='R'){
-                a[T_A]=sumA;
-                sumA=sumA+1;
-            }
-            else{
-                a[T_A]=sumA;
-                sumA=sumA-1;
-            }
-            T_A++;
+        for (int j = 0; j < t; j++) {
+            sumA += (d == 'R') ? 1 : -1;
+            a.push_back(sumA);
         }
+        timeA += t;
     }
 
     for (int i = 0; i < m; i++) {
         cin >> t_b >> d_b;
-        timeB+=t_b;
-        for(int i=0; i<t_b; i++){
-            if(d_b=='R'){
-                b[T_B]=sumB;
-                sumB=sumB+1;
-            }
-            else{
-                b[T_B]=sumB;
-                sumB=sumB-1;
-            }
-            T_B++;
+        for (int j = 0; j < t_b; j++) {
+            sumB += (d_b == 'R') ? 1 : -1;
+            b.push_back(sumB);
         }
-    }
-    int N=max(timeA,timeB);
-    if(timeA>timeB){
-        for(int i=0; i<timeA-timeB; i++){
-            b[T_B]=sumB;
-            T_B++;
-        }
-    }
-    else if(timeA<timeB){
-        for(int i=0; i<timeB-timeA; i++){
-            a[T_A]=sumA;
-            T_A++;
-        }
+        timeB += t_b;
     }
 
-    int cnt=0;
+    int maxTime = max(timeA, timeB);
 
-    if(a[1]==b[1]){
-        cnt++;
+    while (a.size() < maxTime) a.push_back(a.back());
+    while (b.size() < maxTime) b.push_back(b.back());
+
+    int cnt = 0;
+
+    // 선두 교체 판단 (첫 순간 체크)
+    if (a[1] == b[1]) cnt++;
+
+    for (int i = 2; i < maxTime; i++) {
+        if (a[i - 1] != b[i - 1] && a[i] == b[i]) cnt++;
     }
-    for(int i=2; i<=N; i++){
-        //cout<<a[i]<<" "<<b[i]<<endl;
-        if(a[i-1]!=b[i-1] && a[i]==b[i]){
-            cnt++;
-        }
-    }
-    cout<<cnt;
 
-
+    cout << cnt;
     return 0;
-
 }
