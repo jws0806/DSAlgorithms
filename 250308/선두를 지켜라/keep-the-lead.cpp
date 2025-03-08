821f5c89e5c97f52d9b2d015f93e9ea787c0d402
jws0806 <102636345+jws0806@users.noreply.github.com>
2025-03-08 12:00:05 +0900

    Title: 선두를 지켜라, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250308/선두를 지켜라/keep-the-lead.cpp
+++ 250308/선두를 지켜라/keep-the-lead.cpp
@@ -31,14 +31,16 @@ int main() {
         }
     }
     long cnt=0;
-    for(int i=0; i<s1.size()-1; i++){
-        // cout<<s1[i]<<" "<<s2[i]<<endl;
-        if ((s1[i] >= s2[i] && s1[i + 1] < s2[i + 1]) || 
-            (s1[i] <= s2[i] && s1[i + 1] > s2[i + 1])) {
+     int prev_lead = (s1[0] > s2[0]) ? 1 : (s1[0] < s2[0]) ? 2 : 0; // 1: A, 2: B, 0: 동률
+
+    for (int i = 1; i < s1.size(); i++) {
+        int current_lead = (s1[i] > s2[i]) ? 1 : (s1[i] < s2[i]) ? 2 : prev_lead;
+
+        if (current_lead != prev_lead && current_lead != 0) {
             cnt++;
         }
+        prev_lead = current_lead;
     }
-
     cout<<cnt;
 
     // Please write your code here.
