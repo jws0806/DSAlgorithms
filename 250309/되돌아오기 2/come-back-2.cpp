02c06a338d6e33d1920d8da7ed865ec3226997ab
jws0806 <102636345+jws0806@users.noreply.github.com>
2025-03-09 14:22:17 +0900

    Title: 되돌아오기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250309/되돌아오기 2/come-back-2.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string commands;
+int x=0;
+int y=0;
+
+int main() {
+    cin >> commands;
+
+    int way=0;
+    int cnt=0;
+
+    for(int i=0; i<commands.size(); i++){
+        if(commands[i]=='F'){
+            if(way==0){
+                y++;
+
+            }
+            else if(way==1){
+                x++;
+            }
+            else if(way==2){
+                y--;
+            }
+            else if(way==3){
+                x--;
+            }
+
+        }
+        else if(commands[i]=='R'){
+            way++;
+            if(way==4) way=0;
+        }
+        cnt++;
+        //cout<<x<<" "<<y<<endl;
+        if(x==0 && y==0){
+            cout<<cnt;
+            return 0;
+        }
+
+    }
+    cout<<-1;
+
+    // Please write your code here.
+
+    return 0;
+}
