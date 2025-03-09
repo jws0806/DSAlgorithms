f9d6d5aeb0bfd5237446111058c1a3572198737c
jws0806 <102636345+jws0806@users.noreply.github.com>
2025-03-10 08:59:43 +0900

    Title: 이상한 진수 2, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250310/이상한 진수 2/awkward-digits-2.cpp
+++ 250310/이상한 진수 2/awkward-digits-2.cpp
@@ -2,6 +2,7 @@
 #include <cmath>
 #include <string>
 #include <algorithm>
+#include <vector>
 
 using namespace std;
 
@@ -10,22 +11,36 @@ string a;
 int main() {
     cin >> a;
 
-    // Please write your code here.
+    bool flag=true;
+
     for(int i=0; i<a.size(); i++){
         if(a[i] =='0'){
             a[i] ='1';
+            flag=false;
             break;
         }
+    }
+    string ans;
+    if(flag==true){
+        
+        ans[0]='1';
+        for(int i=0; i<a.size(); i++){
+            ans[i+1]='0';
+        }
+        cout<<ans;
+        return 0;
     }
     // for(int i=0; i<a.size(); i++){
     //     cout<<a[i];
     // }
-    int sum=0;
-    reverse(a.begin(),a.end());
-    for(int i=0; i<a.size(); i++){
-        sum+=(a[i]-'0')*pow(2,i);
-    }
-    cout<<sum;
+    else{
+        int sum=0;
+        reverse(a.begin(),a.end());
+        for(int i=0; i<a.size(); i++){
+            sum+=(a[i]-'0')*pow(2,i);
+            }
+            cout<<sum;
+        }
 
     return 0;
 }
