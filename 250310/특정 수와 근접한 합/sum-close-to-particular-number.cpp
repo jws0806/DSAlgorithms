57c8c21e757fd0a68c7df8af23ce7d438822cde6
jws0806 <102636345+jws0806@users.noreply.github.com>
2025-03-10 21:03:32 +0900

    Title: 특정 수와 근접한 합, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 250310/특정 수와 근접한 합/sum-close-to-particular-number.cpp
+++ 250310/특정 수와 근접한 합/sum-close-to-particular-number.cpp
@@ -17,14 +17,14 @@ int main() {
     int min=100000;
     for(int i=0; i<N-1; i++){
         for(int j=i+1; j<N; j++){
-            int num=abs(sum-arr[i]-arr[j]);
+            int num=abs(S-(sum-arr[i]-arr[j]));
             //cout<<num<<" ";
             if(min>num){
                 min=num;
             }
         }
     }
-    cout<<abs(S-min);
+    cout<<min;
 
     // Please write your code here.
 
