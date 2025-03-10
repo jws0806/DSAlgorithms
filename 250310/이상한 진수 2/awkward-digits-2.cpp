bcfc1af2d6692253b249702ccebbae446c39dbee
jws0806 <102636345+jws0806@users.noreply.github.com>
2025-03-10 09:04:57 +0900

    Title: 이상한 진수 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 250310/이상한 진수 2/awkward-digits-2.cpp
+++ 250310/이상한 진수 2/awkward-digits-2.cpp
@@ -2,45 +2,43 @@
 #include <cmath>
 #include <string>
 #include <algorithm>
-#include <vector>
 
 using namespace std;
 
-string a;
-
 int main() {
-    cin >> a;
+    string a;
+    cin >> a;  // 이진수 문자열 입력 받기
 
-    bool flag=true;
+    bool flag = true;
 
-    for(int i=0; i<a.size(); i++){
-        if(a[i] =='0'){
-            a[i] ='1';
-            flag=false;
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] == '0') {
+            a[i] = '1';
+            flag = false;
             break;
         }
     }
-    string ans;
-    if(flag==true){
-        
-        ans[0]='1';
-        for(int i=0; i<a.size(); i++){
-            ans[i+1]='0';
-        }
-        cout<<ans;
+
+    if (flag) {
+        int n=a.size();
+        a[n-1]='0';
+        int sum = 0;
+        reverse(a.begin(), a.end());  
+        for (int i = 0; i < a.size(); i++) {
+            sum += (a[i] - '0') * (1 << i);
+        }  
+        cout<<sum;
         return 0;
     }
-    // for(int i=0; i<a.size(); i++){
-    //     cout<<a[i];
-    // }
-    else{
-        int sum=0;
-        reverse(a.begin(),a.end());
-        for(int i=0; i<a.size(); i++){
-            sum+=(a[i]-'0')*pow(2,i);
-            }
-            cout<<sum;
-        }
 
+
+
+    int sum = 0;
+    reverse(a.begin(), a.end());  
+    for (int i = 0; i < a.size(); i++) {
+        sum += (a[i] - '0') * (1 << i);  // (1 << i) == pow(2, i)
+    }
+
+    cout << sum << endl;
     return 0;
 }
