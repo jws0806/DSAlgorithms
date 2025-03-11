65beccc9f879e7ea05111e1b53538732ac1a7f27
jws0806 <102636345+jws0806@users.noreply.github.com>
2025-03-11 19:28:35 +0900

    Title: 구간 중 최대 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250311/구간 중 최대 합/max-sum-of-subarray.cpp
+++ 250311/구간 중 최대 합/max-sum-of-subarray.cpp
@@ -11,7 +11,7 @@ int main() {
         cin >> arr[i];
     }
     int Max=0;
-    for(int i=0; i<n-k; i++){
+    for(int i=0; i<=n-k; i++){
         int sum=0;
         for(int j=i; j<i+k; j++){
             sum=sum+arr[j];
