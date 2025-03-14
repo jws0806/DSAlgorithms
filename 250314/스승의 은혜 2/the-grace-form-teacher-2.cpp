b7723ad14a0167d53b2a9e3b579c5f56ca9967e6
jws0806 <102636345+jws0806@users.noreply.github.com>
2025-03-14 20:48:18 +0900

    Title: 스승의 은혜 2, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250314/스승의 은혜 2/the-grace-form-teacher-2.cpp
+++ 250314/스승의 은혜 2/the-grace-form-teacher-2.cpp
@@ -7,34 +7,34 @@ int P[1000];
 
 int main() {
     cin >> N >> B;
-
     for (int i = 0; i < N; i++) {
         cin >> P[i];
     }
-
     int Max=0;
     for(int i=0; i<N; i++){
         int sum=0;
         int cnt=0;
-        for(int j=0; (j<N || sum<=B); j++){
+        for(int j=0; j<N; j++){
            
                 if(i==j){
                     sum+=P[j]/2;
                     cnt++;
+                    if(sum>B){
+                        cnt--;
+                        break;
+
+                    }
                 }
                 else{
                     sum+=P[j];
                     cnt++;
+                    if(sum>B){
+                        cnt--;
+                    }
                 }
         }
         Max=max(cnt,Max);
-   
-
     }
-     cout<<Max-1;
-
-
-    // Please write your code here.
-
+    cout<<Max;
     return 0;
 }
