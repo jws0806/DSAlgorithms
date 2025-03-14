81b007165f7f00d663c6828f1f2f85a9c5c15f82
jws0806 <102636345+jws0806@users.noreply.github.com>
2025-03-14 21:21:18 +0900

    Title: 운행 되고 있는 시간, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250314/운행 되고 있는 시간/hours-in-service.cpp
+++ 250314/운행 되고 있는 시간/hours-in-service.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int N;
 int A[100], B[100];
-vector<int>v(1000,0);
+vector<int>v(1001,0);
 
 int main() {
     cin >> N;
@@ -14,15 +14,19 @@ int main() {
         cin >> A[i] >> B[i];
     }
     for(int i=0; i<N; i++){
-        for(int j=A[i]; j<=B[i]; j++){
+        for(int j=A[i]; j<B[i]; j++){
             v[j]++;
         }
     }
+    //  for(int i=0; i<10; i++){
+    //          cout<<v[i]<<" ";   
+    //  }
+
     int Max=0;
     for(int i=0; i<N; i++){
         int cnt=0;
     
-        for(int j=A[i]; j<=B[i]; j++){
+        for(int j=A[i]; j<B[i]; j++){
             v[j]--;
         }
         for(int k=0; k<=1000; k++){
@@ -32,7 +36,7 @@ int main() {
         }
         Max=max(cnt,Max);
     }
-    cout<<Max;
+    cout<<Max+1;
 
     return 0;
 }
