9382ffeee1209e828c3b7567ac50b435c53e37d4
jws0806 <102636345+jws0806@users.noreply.github.com>
2025-03-14 21:01:02 +0900

    Title: 스승의 은혜 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 250314/스승의 은혜 2/the-grace-form-teacher-2.cpp
+++ 250314/스승의 은혜 2/the-grace-form-teacher-2.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <cmath>
+#include <algorithm>
 using namespace std;
 
 int N, B;
@@ -10,29 +10,27 @@ int main() {
     for (int i = 0; i < N; i++) {
         cin >> P[i];
     }
-    int Max=0;
-    for(int i=0; i<N; i++){
-        int sum=0;
-        int cnt=0;
-        for(int j=0; j<N; j++){
-            int price;
-            if(i==j){
-                price=P[j]/2;   
-            }
-            else {
-                price=P[j];
-            }
 
-            if(sum+price<=B){
-                sum+=price;
+    sort(P, P + N); // 상품 가격을 오름차순 정렬
+    int Max = 0;
+
+    for (int i = 0; i < N; i++) { 
+        int sum = 0, cnt = 0;
+        
+        // i번째 상품을 할인할 경우
+        for (int j = 0; j < N; j++) {
+            int price = (i == j) ? P[j] / 2 : P[j]; // 특정 상품만 할인 적용
+
+            if (sum + price <= B) {
+                sum += price;
                 cnt++;
-            }
-            else{
-                break;
+            } else {
+                break; // 예산 초과 시 종료
             }
         }
-        Max=max(cnt,Max);
+        Max = max(Max, cnt); // 최대 구매 개수 갱신
     }
-    cout<<Max;
+
+    cout << Max;
     return 0;
 }
