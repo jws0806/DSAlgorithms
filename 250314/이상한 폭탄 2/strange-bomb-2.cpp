fa12857f6ede85a684b66ae2cd9b95a68410cf7b
jws0806 <102636345+jws0806@users.noreply.github.com>
2025-03-14 09:33:21 +0900

    Title: 이상한 폭탄 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250314/이상한 폭탄 2/strange-bomb-2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int N, K;
+int num[100];
+
+int main() {
+    cin >> N >> K;
+
+    int Max=0;
+
+    for (int i = 0; i < N; i++) {
+        cin >> num[i];
+    }
+    for(int i=0; i<N; i++){
+        for(int j=i+1; (j<i+K || j<N); j++){
+            if(num[j]==num[i]){
+                if(Max<num[j]){
+                    Max=num[j];
+                }
+            }
+        }
+    }
+    if(Max==0) cout<<-1;
+    else cout<<Max;
+
+
+
+    return 0;
+}
