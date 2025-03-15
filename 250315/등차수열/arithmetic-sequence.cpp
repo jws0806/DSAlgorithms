aba563dcb42cff99cc816fc2837d63a6079c1b40
jws0806 <102636345+jws0806@users.noreply.github.com>
2025-03-15 21:33:13 +0900

    Title: 등차수열, Time: 11ms, Memory: 0MB, Status: Passed - CodeTree

--- 250315/등차수열/arithmetic-sequence.cpp
+++ 250315/등차수열/arithmetic-sequence.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <algorithm>
+#include <unordered_map>
 
 using namespace std;
 
@@ -11,19 +12,28 @@ int main() {
     for (int i = 0; i < n; i++) {
         cin >> a[i];
     }
+
+    sort(a, a + n); // 정렬 (이진 탐색을 활용할 수도 있음)
+
+    int maxCount = 0;
     
-    sort(a, a + n); // 정렬
-
-    int cnt = 0;
-
     for (int i = 0; i < n - 1; i++) {
         for (int j = i + 1; j < n; j++) {
-            if ((a[i] + a[j]) % 2 == 0) { // k가 정수인지 확인
-                cnt++;
+            if ((a[i] + a[j]) % 2 == 0) { // K가 정수인지 확인
+                int K = (a[i] + a[j]) / 2;
+
+                // (aᵢ, K, aⱼ) 구조를 만족하는 개수 세기
+                int count = 0;
+                for (int x = 0; x < n; x++) {
+                    for (int y = x + 1; y < n; y++) {
+                        if ((a[x] + a[y]) == 2 * K) count++;
+                    }
+                }
+                maxCount = max(maxCount, count);
             }
         }
     }
 
-    cout << cnt;
+    cout << maxCount;
     return 0;
 }
