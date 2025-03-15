3fc7e786bd258770e5b30b05301d87fc6d719079
jws0806 <102636345+jws0806@users.noreply.github.com>
2025-03-15 10:27:08 +0900

    Title: 흥미로운 숫자 2, Time: 13ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250315/흥미로운 숫자 2/interesting-numbers-2.cpp
+++ 250315/흥미로운 숫자 2/interesting-numbers-2.cpp
@@ -25,7 +25,7 @@ int main() {
             if(num[j]==1){
                 cnt_one++;
             }
-            if(num[j]>=1){
+            if(num[j]>0){
                 cnt++;
             }
         }
