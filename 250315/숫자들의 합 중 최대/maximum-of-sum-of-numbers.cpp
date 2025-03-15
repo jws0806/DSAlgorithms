8bd398de8bedf5543a4fa1ed507bd002dbb2af57
jws0806 <102636345+jws0806@users.noreply.github.com>
2025-03-15 09:50:33 +0900

    Title: 숫자들의 합 중 최대, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250315/숫자들의 합 중 최대/maximum-of-sum-of-numbers.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int X, Y;
+
+int main() {
+    int Max=0;
+    cin >> X >> Y;
+
+    for(int i=X; i<=Y; i++){
+        int num=X;
+        int sum=0;
+        while(num>0){
+            sum=sum+num%10;
+            num=num/10;
+        }
+        Max=max(sum,Max);
+    }
+    cout<<Max;
+
+    return 0;
+}
