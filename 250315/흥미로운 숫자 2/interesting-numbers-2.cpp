147fa351d93f60bc17f98baa02ce50686a80ed05
jws0806 <102636345+jws0806@users.noreply.github.com>
2025-03-15 10:25:23 +0900

    Title: 흥미로운 숫자 2, Time: 12ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250315/흥미로운 숫자 2/interesting-numbers-2.cpp
+++ 250315/흥미로운 숫자 2/interesting-numbers-2.cpp
@@ -16,13 +16,20 @@ int main() {
             num[n%10]++;
             n=n/10;
         }
+        // for(int k=0; k<10; k++){
+        //     cout<<num[k];
+        // }
         int cnt=0;
+        int cnt_one=0;
         for(int j=0; j<10; j++){
-            if(num[j]>0){
+            if(num[j]==1){
+                cnt_one++;
+            }
+            else if(num[j]>1){
                 cnt++;
             }
         }
-        if(cnt==2){
+        if(cnt_one==1 && cnt>0){
             one_count++;
         }
         //cout<<endl;
