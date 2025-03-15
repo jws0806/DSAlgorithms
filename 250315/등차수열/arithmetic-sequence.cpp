cc565f0970e8dc69e9e835afaa73b1c2ce17031f
jws0806 <102636345+jws0806@users.noreply.github.com>
2025-03-15 21:08:57 +0900

    Title: 등차수열, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250315/등차수열/arithmetic-sequence.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int n;
+int a[100];
+
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    sort(a,a+n);
+    int Min=a[0];
+    int Max=a[n-1];
+
+    int k=Min;
+    int cnt=0;
+
+    while(k<Max){
+        for(int i=0; i<n-1; i++){
+            for(int j=i+1; j<n; j++){
+                if(a[j]-k==k-a[i]){
+                    cnt++;
+                }
+            }
+        }
+        k++;
+    }
+    cout<<cnt;
+    return 0;
+}
