9c2a5c978cf804218f78887b3c126e2f2df168ea
jws0806 <102636345+jws0806@users.noreply.github.com>
2025-03-18 22:07:49 +0900

    Title: 두 선분, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250318/두 선분/two-lines.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int x1, x2, x3, x4;
+
+int main() {
+    cin >> x1 >> x2 >> x3 >> x4;
+
+    if(x1<x3 && x3<x2){
+        cout<<"intersecting";
+    }
+    else{
+        cout<<"nonintersecting";
+    }
+
+    // Please write your code here.
+
+    return 0;
+}
